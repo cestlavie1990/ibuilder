ff11b6a3de546a1231b64653ef1b8116723115e4
Mr.Minakov <Mr.Minakov@DESKTOP-21C13RF>
2017-07-13 08:45:43 +0300

    Страницы перенесены в закрытый раздел

--- src/java/controller/PlacesServlet.java
+++ src/java/controller/PlacesServlet.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +12,14 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Mr.Minakov
  */
+@ServletSecurity(
+        @HttpConstraint(rolesAllowed = {"admin"}))
 public class PlacesServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        request.getRequestDispatcher("WEB-INF/views/places.jsp").forward(request, response);
+        request.getRequestDispatcher("WEB-INF/private/places.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
